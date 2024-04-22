0f1da5a85416340102ca5e75a71732c4918a4758
aleksandrayefimova <123464076+aleksandrayefimova@users.noreply.github.com>
2024-04-22 23:06:17 +0300

    fix caption 1-gallery.js

--- src/js/1-gallery.js
+++ src/js/1-gallery.js
@@ -1,69 +1,32 @@
-// Описаний в документації
-import SimpleLightbox from 'simplelightbox';
-// Додатковий імпорт стилів
-import 'simplelightbox/dist/simple-lightbox.min.css';
 
+// Імпорт бібліотеки SimpleLightbox як залежність проекту
+import SimpleLightbox from "simplelightbox";
+// Додатковий імпорт стилів
+import "simplelightbox/dist/simple-lightbox.min.css";
 // Add imports above this line
 import { images } from './gallery-items';
+// Change code below this line
+const galleryElements = document.querySelector('.gallery');
 
+// Формуємо розмітку галереї на основі масиву даних
+const createGalleryMarkup = images => {
+    return images
+    .map(({ preview, original, description }) => {
+        return `
+            <a class="gallery__item" href=${original}>
+                <img class="gallery__image" src=${preview} alt=${description} />
+            </a>`;
+    })
+    .join('');
+};
 
-const ulGallary = document.querySelector(".gallery");
+// Рендеримо розмітку галереї
+galleryElements.insertAdjacentHTML('beforeend', createGalleryMarkup(images));
 
-
-function renderGallery(arr) {
-    if (!Array.isArray(arr)) {
-        return null;
-    }
-    
-    const html = arr.map(item => htmlTemplate(item)).join("");
-
-    ulGallary.innerHTML = html;
-}
-
-function htmlTemplate({ preview, original, description }) {
-    return `
-    <li class="gallery__item">
-        <a class="gallery__link" href="${original}">
-            <img
-                class="gallery__image"
-                src="${preview}"
-                data-source="${original}"
-                alt="${description}"
-            />
-        </a>
-    </li>`;
-}
-
-renderGallery(images);
-
-const captionDelay = 250
-
-const lightbox = new SimpleLightbox('.gallery a', { 
-    captionDelay, 
-    captions: true,
-    captionPosition: "bottom",
-    captionData: "title",
-    additionalHtml: ` `,
+const lightbox = new SimpleLightbox('.gallery a', {
+    overlayOpacity: 0.5,
+    captionDelay: 250,
+    captionsData: 'alt',
+    fadeSpeed: 250,
+    rtl: true,
 });
-
-function showCaption(event) {
-    const captionElement = document.querySelector('.sl-additional-html');
-    captionElement.textContent = event.target.firstElementChild.alt;
-
-    setTimeout(() => {
-        captionElement.classList.add('show');
-    }, captionDelay);
-}
-
-function hideCaption() {
-    const captionElement = document.querySelector('.sl-additional-html');
-    captionElement.classList.remove('show');
-}
-
-lightbox.on('shown.simplelightbox', showCaption);
-
-lightbox.on('changed.simplelightbox', function(event) {
-    hideCaption();
-    showCaption(event);
-});
-
